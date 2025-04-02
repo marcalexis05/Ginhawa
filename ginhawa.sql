146564fbec5ed6c3c6a8dfd3a5fb3c177aaf99ce
marcalexis05 <alexismarc066@gmail.com>
2025-04-02 23:18:40 +0800

    detabes

--- ginhawa.sql
+++ ginhawa.sql
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `admins`
 --
 
 CREATE TABLE `admin` (
